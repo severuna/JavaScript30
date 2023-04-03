ee34787f087811d6d7722f42118c3137b1e088e3
Severuna <oxxxyfomenkova@gmail.com>
2023-04-03 22:45:56 +0300

    set inputs event listener

--- 03_CSS_Variables/script/index.js
+++ 03_CSS_Variables/script/index.js
@@ -2,4 +2,11 @@ document.addEventListener("DOMContentLoaded", ( ) => {
     
     const option = document.querySelectorAll("input");
     
+    option.forEach( el => {
+        el.addEventListener('input', () => {
+            const suffix = el.dataset.sizing || '';
+            document.documentElement.style.setProperty(`--${el.name}`, el.value + suffix);
+        });
+    });
+    
 });
