03fe56978c1704a3514b3f4e8ed8f3d7254533e8
Severuna <oxxxyfomenkova@gmail.com>
2023-04-03 17:18:33 +0300

    set func setDate logic

--- 02_JS_and_CSS_Clock/script/index.js
+++ 02_JS_and_CSS_Clock/script/index.js
@@ -1,7 +1,27 @@
 document.addEventListener("DOMContentLoaded", ( ) => {
-    
+
     const hour = document.querySelector("#hour");
     const min = document.querySelector("#min");
     const second = document.querySelector("#second");
 
+    function setDate() {
+
+        const now = new Date();
+
+        const sec = now.getSeconds();
+
+        const secondsDeg = ((sec / 60) * 360) + 90;
+
+        second.style.transform = `rotate(${secondsDeg}deg)`;
+
+        const mins = now.getMinutes();
+
+        const minsDeg = ((mins / 60) * 360) + ((sec/60)*6) + 90;
+
+        min.style.transform = `rotate(${minsDeg}deg)`;
+
+        const hours = now.getHours();
+        const hoursDeg = ((hours / 12) * 360) + ((mins/60)*30) + 90;
+        hour.style.transform = `rotate(${hoursDeg}deg)`;
+    }
 });
