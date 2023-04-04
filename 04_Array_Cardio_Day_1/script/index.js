95432e1e8393b018dc5014387de5fadfe94e93e8
Severuna <oxxxyfomenkova@gmail.com>
2023-04-04 23:35:20 +0300

    create people arr

--- 04_Array_Cardio_Day_1/script/index.js
+++ 04_Array_Cardio_Day_1/script/index.js
@@ -16,4 +16,16 @@ document.addEventListener("DOMContentLoaded", () => {
         { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
     ];
 
+    // create people arr
+    const people = [
+        'Bernhard, Sandra', 'Bethea, Erin', 'Becker, Carl', 'Bentsen, Lloyd', 'Beckett, Samuel', 'Blake, William',
+        'Berger, Ric', 'Beddoes, Mick', 'Beethoven, Ludwig', 'Belloc, Hilaire', 'Begin, Menachem', 'Bellow, Saul',
+        'Benchley, Robert', 'Blair, Robert', 'Benenson, Peter', 'Benjamin, Walter', 'Berlin, Irving', 'Benn, Tony',
+        'Benson, Leana', 'Bent, Silas', 'Berle, Milton', 'Berry, Halle', 'Biko, Steve', 'Beck, Glenn', 
+        'Bergman,Ingmar', 'Black, Elk', 'Berio, Luciano', 'Berne, Eric', 'Berra, Yogi', 'Berry, Wendell', 
+        'Bevan, Aneurin', 'Ben-Gurion, David', 'Bevel, Ken', 'Biden, Joseph', 'Bennington, Chester', 
+        'Bierce, Ambrose', 'Billings, Josh', 'Birrell, Augustine', 'Blair, Tony', 'Beecher, Henry', 'Biondo, Frank'
+    ];
+
+
 });
