4dc1ffe2028c92cb920a6bf8a0426f53f320e78d
Severuna <oxxxyfomenkova@gmail.com>
2023-04-16 22:01:29 +0300

    add panel event listeners ( click & transitionend )

--- 05_Flex_Panel_Gallery/script/index.js
+++ 05_Flex_Panel_Gallery/script/index.js
@@ -3,5 +3,29 @@ document.addEventListener("DOMContentLoaded", ( ) => {
     console.log(`the page is loaded`);
 
     const panels = document.querySelectorAll('.panel');
-    
+
+    panels.forEach( panel => {
+
+        panel.addEventListener("click", ( e )=> {
+
+            e.preventDefault();
+
+            panel.classList.toggle('open');
+
+        });
+
+        panel.addEventListener('transitionend', ( e ) => {
+
+            e.preventDefault();
+
+            if (e.propertyName.includes('flex')) {
+
+                panel.classList.toggle('open-active');
+
+            }
+
+        });
+
+    });
+
 });
