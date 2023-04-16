ad0db1b7b5b5cefa59620395e8078e4443bbab8c
Severuna <oxxxyfomenkova@gmail.com>
2023-04-16 21:52:51 +0300

    call all panel

--- 05_Flex_Panel_Gallery/script/index.js
+++ 05_Flex_Panel_Gallery/script/index.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", ( ) => {
-    // init message
+    
     console.log(`the page is loaded`);
+
+    const panels = document.querySelectorAll('.panel');
+    
 });
