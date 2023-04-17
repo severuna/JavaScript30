0f46fb957dbd4e2b5e54c56aaced9e2c10973c6a
Severuna <oxxxyfomenkova@gmail.com>
2023-04-17 21:51:20 +0300

    add init data arrs people & comments

--- 07_Array_Cardio_Day_2/script/index.js
+++ 07_Array_Cardio_Day_2/script/index.js
@@ -2,4 +2,50 @@ document.addEventListener("DOMContentLoaded", ( ) => {
     
     console.log(`the page is loaded`);
 
+    // init data arr people
+
+    const people = [
+        { 
+            name: 'Wes', 
+            year: 1988 
+        },
+        { 
+            name: 'Kait', 
+            year: 1986 
+        },
+        { 
+            name: 'Irv', 
+            year: 1970 
+        },
+        { 
+            name: 'Lux', 
+            year: 2015 
+        }
+    ];
+
+    // init data arr comments
+    
+    const comments = [
+        { 
+            text: 'Love this!', 
+            id: 523423 
+        },
+        { 
+            text: 'Super good', 
+            id: 823423 
+        },
+        { 
+            text: 'You are the best', 
+            id: 2039842 
+        },
+        { 
+            text: 'Ramen is my fav food ever', 
+            id: 123523 
+        },
+        { 
+            text: 'Nice Nice Nice!', 
+            id: 542328 
+        }
+    ];
+
 });
